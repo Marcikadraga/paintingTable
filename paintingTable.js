065711ddc2c15bab196d1ae4433b6052d15d4c8c
Marci <marciprog@gmail.com>
2021-02-16 12:35:39 +0100

    Deleted almost everything and code is just working fine

--- paintingTable.js
+++ paintingTable.js
@@ -1,80 +1,83 @@
-//var fieldPosition={id=0,x=0,y=0}
-class Field {    
-    _id;
-    _field;
-    _fieldSize;
-    _ctx;
-    _fieldPosition;
-    
-    get id() {
-        return this._id;
-    }
-    set id(value) {
-        this._id = value;
-    }
-    get field() {
-        return this._field;
-    }
-    set field(value) {
-        this._field = value;
-    }
-    get fieldSize() {
-        return this._fieldSize;
-    }
-    set fieldSize(value) {
-        this._fieldSize = value;
-    }
-    get ctx() {
-        return this._ctx;
-    }
-    set ctx(value) {
-        this._ctx = value;
-    }
-    get fieldPosition() {
-        return this._fieldPosition;
-    }
-    set fieldPosition(value) {
-        this._fieldPosition = value;
+
+class FieldPosition {
+    // x; y; id =0;
+    constructor(y, x, id) {
+        this.y = y;
+        this.x = x;
+        this.id = id;
     }
 
-    constructor(id, yFieldPosition, xFieldPosition) {
-        this.id = id
+
+}
+class Field {    
+    
+    // _field;
+    // _fieldSize;
+    // _ctx;
+    // _fieldPosition;
+    
+   
+    // get field() {
+    //     return this._field;
+    // }
+    // set field(value) {
+    //     this._field = value;
+    // }
+    // get fieldSize() {
+    //     return this._fieldSize;
+    // }
+    // set fieldSize(value) {
+    //     this._fieldSize = value;
+    // }
+    // get ctx() {
+    //     return this._ctx;
+    // }
+    // set ctx(value) {
+    //     this._ctx = this.field.getContext("2d");
+    // }
+    // get fieldPosition() {
+    //     return this._fieldPosition;
+    // }
+    // set fieldPosition(value) {
+    //     this._fieldPosition = value;
+    // }
+
+    constructor(FieldPosition) {
         this.fieldSize = 20;
         this.field = document.createElement('canvas');
-        this.ctx = this.field.getContext("2d");
-        this.field.id = id;
+        this.field.id =FieldPosition.id;
         this.field.style.width = this.fieldSize + "px";
         this.field.style.height = this.fieldSize + "px";
-        this.field.style.left = this.fieldSize * xFieldPosition + "px";
-        this.field.style.top = this.fieldSize * yFieldPosition + "px";
+        this.field.style.left = this.fieldSize * FieldPosition.x + "px";
+        this.field.style.top = this.fieldSize * FieldPosition.y + "px";
         this.field.style.position = "absolute";
         this.fieldPosition=document.getElementById("snakeTable").appendChild(this.field);
     }
 }
 class Table {
-    _array;
-    _yTableSize;
-    _xTableSize;
+    // _array;
+    // _yTableSize;
+    // _xTableSize;
 
-    get array() {
-        return this._array;
-    }
-    set array(value) {
-        this._array = value;
-    }
-    get yTableSize() {
-        return this._yTableSize;
-    }
-    set yTableSize(value) {
-        this._yTableSize = value;
-    }
+    // get array() {
+    //     return this._array;
+    // }
+    // set array(value) {
+    //     this._array = value;
+    // }
+    // get yTableSize() {
+    //     return this._yTableSize;
+    // }
+    // set yTableSize(value) {
+    //     this._yTableSize = value;
+    // }
 
-    get xTableSize() {
-        return this._xTableSize;
-    }
-    set xTableSize(value) {
-        this._xTableSize = value;
-    }
+    // get xTableSize() {
+    //     return this._xTableSize;
+    // }
+    // set xTableSize(value) {
+    //     this._xTableSize = value;
+    // }
 
 
     constructor(yTableSize, xTableSize) {
@@ -84,12 +87,12 @@ class Table {
     }
 
     DisplayTheTable() {
-        var id = 0;
+        var id =0;
         for (var i = 0; i < this.yTableSize; i++) {
             this.array.push([]);
             for (var j = 0; j < this.xTableSize; j++) {
-                this.array.push(new Field(id, i, j))
-                id++;
+                this.array.push(new Field(new FieldPosition(i,j,id)))
+                id++
             }
         }
         for (var i = 0; i < this.yTableSize * this.xTableSize; i++) {
@@ -112,8 +115,3 @@ class Main
 }
 var m=new Main();
 m.startTheProgram();
-
-
-
-
-
