5d026db7feb7fa4175ce8ff66071a2832557ee7f
Marci <marciprog@gmail.com>
2021-02-16 22:39:31 +0100

    changed a lot of things but everything works fine

--- paintingTable.js
+++ paintingTable.js
@@ -1,98 +1,40 @@
 
 class FieldPosition {
-    // x; y; id =0;
     constructor(y, x, id) {
         this.y = y;
         this.x = x;
         this.id = id;
     }
-
-
 }
-class Field {    
-    
-    // _field;
-    // _fieldSize;
-    // _ctx;
-    // _fieldPosition;
-    
-   
-    // get field() {
-    //     return this._field;
-    // }
-    // set field(value) {
-    //     this._field = value;
-    // }
-    // get fieldSize() {
-    //     return this._fieldSize;
-    // }
-    // set fieldSize(value) {
-    //     this._fieldSize = value;
-    // }
-    // get ctx() {
-    //     return this._ctx;
-    // }
-    // set ctx(value) {
-    //     this._ctx = this.field.getContext("2d");
-    // }
-    // get fieldPosition() {
-    //     return this._fieldPosition;
-    // }
-    // set fieldPosition(value) {
-    //     this._fieldPosition = value;
-    // }
+class Field {
 
     constructor(FieldPosition) {
 
         this.fieldSize = 20;
         this.field = document.createElement('canvas');
-        this.field.id =FieldPosition.id;
+        this.field.id = FieldPosition.id;
         this.field.style.width = this.fieldSize + "px";
         this.field.style.height = this.fieldSize + "px";
         this.field.style.left = this.fieldSize * FieldPosition.x + "px";
         this.field.style.top = this.fieldSize * FieldPosition.y + "px";
         this.field.style.position = "absolute";
-        this.fieldPosition=document.getElementById("table").appendChild(this.field);
+        this.fieldPosition = document.getElementById("drawingTable").appendChild(this.field);
     }
 }
 class Table {
-    // _array;
-    // _yTableSize;
-    // _xTableSize;
-
-    // get array() {
-    //     return this._array;
-    // }
-    // set array(value) {
-    //     this._array = value;
-    // }
-    // get yTableSize() {
-    //     return this._yTableSize;
-    // }
-    // set yTableSize(value) {
-    //     this._yTableSize = value;
-    // }
-
-    // get xTableSize() {
-    //     return this._xTableSize;
-    // }
-    // set xTableSize(value) {
-    //     this._xTableSize = value;
-    // }
-
 
     constructor(yTableSize, xTableSize) {
-        this.array=[];
+        this.array = [];
         this.yTableSize = yTableSize;
         this.xTableSize = xTableSize;
     }
 
     DisplayTheTable() {
-        var id =0;
+        var id = 0;
         for (var i = 0; i < this.yTableSize; i++) {
             this.array.push([]);
             for (var j = 0; j < this.xTableSize; j++) {
-                this.array.push(new Field(new FieldPosition(i,j,id)))
+                this.array.push(new Field(new FieldPosition(i, j, id)))
                 id++
             }
         }
@@ -104,64 +46,68 @@ class Table {
                 document.getElementById(i).style.backgroundColor = "#EBEFF0";
             }
         }
+        // document.getElementById("table").style.height=this.yTableSize*10+"px";
+        // document.getElementById("drawingTable").style.height=this.yTableSize*10+"px";
+        // document.getElementById("drawingTable").style.width =this.xTableSize*10+"px";
+        
+        
     }
 }
-class ColorFiled{
-    constructor(y,x,id) {
+class ColorFiled {
+    constructor(FieldPosition) {
 
-        this.colorFieldSize = 20;
+        this.colorFieldSize = 40;
         this.colorField = document.createElement('canvas');
-        this.colorField.id =id;
+        this.colorField.id = FieldPosition.id;
         this.colorField.style.width = this.colorFieldSize + "px";
         this.colorField.style.height = this.colorFieldSize + "px";
-        this.colorField.style.left = this.colorFieldSize * x + "px";
-        this.colorField.style.top = this.colorFieldSize * y + "px";
+        this.colorField.style.left = this.colorFieldSize * FieldPosition.x + "px";
+        this.colorField.style.top = this.colorFieldSize * FieldPosition.y + "px";
         this.colorField.style.position = "absolute";
         document.getElementById("colorPalette").appendChild(this.colorField);
     }
 }
-class ColorTable{
-    colorArray=[];
-    colors=[
-         
-         ["#EA2828","#C90011","#A80000","#880000","#6A0000"],
-         ["#E68CE7","#BD66BF","#964299","#701B74","#4B0052"],
-         ["#8C8CE7","#6A6DC4","#484FA2","#233381","#001A62"],
-         ["#A5FFF3","#70C9BE","#3A968C","#00655D","#003831"],
-         ["#7ED19A","#54A672","#287D4C","#005629","#003105"],
-         ["#F0E897","#BCB567","#8A853A","#5A580B","#302F00"],
-         ["#FFD300","#CAA400","#967800","#674F00","#432700"],
-         ["#FF6400","#D64100","#AE1900","#890000","#670000"]
-     ];
-    //  constructor(yTableSize, xTableSize) {
-    //     this.colorArray=[];
-    //     this.yTableSize = yTableSize;
-    //     this.xTableSize = xTableSize;
-    // }
+class ColorTable {
 
 
-    DisplayTheTable() {
-        var id =5000;
-        for (var i = 0; i <this.colors.length ; i++) {
-            this.colorArray.push([]);
+    constructor() {
+        this.colorFields = [];
+        this.colors = [
+            ["#EA2828", "#C90011", "#A80000", "#880000", "#6A0000"],
+            ["#E68CE7", "#BD66BF", "#964299", "#701B74", "#4B0052"],
+            ["#8C8CE7", "#6A6DC4", "#484FA2", "#233381", "#001A62"],
+            ["#A5FFF3", "#70C9BE", "#3A968C", "#00655D", "#003831"],
+            ["#7ED19A", "#54A672", "#287D4C", "#005629", "#003105"],
+            ["#F0E897", "#BCB567", "#8A853A", "#5A580B", "#302F00"],
+            ["#FFD300", "#CAA400", "#967800", "#674F00", "#432700"],
+            ["#FF6400", "#D64100", "#AE1900", "#890000", "#670000"]
+        ];
+    }
+
+
+    DisplayTheColorTable() {
+        var id = 5000;
+        for (var i = 0; i < this.colors.length; i++) {
+            this.colorFields.push([]);
             for (var j = 0; j < this.colors[0].length; j++) {
-                this.colorArray.push(new ColorFiled(i,j,id));
-                document.getElementById(id).style.backgroundColor=this.colors[i][j];
+                this.colorFields.push(new ColorFiled(new FieldPosition( i, j, id)));
+                document.getElementById(id).style.backgroundColor = this.colors[i][j];
                 id++
             }
         }
-        
+        // document.getElementById("colorPalette").style.height=this.colors.length*20+"px";
+        // document.getElementById("colorPalette").style.width=this.colors[0].length*20+"px";
+
     }
 }
-class Main
-{
-    startTheProgram(){
-        var t = new Table(11, 11);
+class Main {
+    startTheProgram() {
+        var t = new Table(35, 35);
         t.DisplayTheTable();
+        var colorTable = new ColorTable();
+        colorTable.DisplayTheColorTable();
     }
-    
 }
-var m=new Main();
+var m = new Main();
 m.startTheProgram();
-var colorTable=new ColorTable();
-colorTable.DisplayTheTable();
+
