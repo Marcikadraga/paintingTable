4c7ca3c3285441c5d1d75c4105b5fb9a49e52abb
Marci <marciprog@gmail.com>
2021-02-19 03:59:44 +0100

    refactoring

--- paintingTable.js
+++ paintingTable.js
@@ -11,17 +11,22 @@ class CurrentColor {
         this.color = color;
     }
 }
+class FieldSize {
+    constructor(fieldSize) {
+        this.fieldSize = fieldSize;
+    }
+}
 class Field {
 
-    constructor(FieldPosition) {
+    constructor(FieldPosition, FieldSize) {
 
-        this.fieldSize = 20;
+        // this.fieldSize = 20;
         this.field = document.createElement('canvas');
         this.field.id = FieldPosition.id;
-        this.field.style.width = this.fieldSize + "px";
-        this.field.style.height = this.fieldSize + "px";
-        this.field.style.left = this.fieldSize * FieldPosition.x + "px";
-        this.field.style.top = this.fieldSize * FieldPosition.y + "px";
+        this.field.style.width = FieldSize.fieldSize + "px";
+        this.field.style.height = FieldSize.fieldSize + "px";
+        this.field.style.left = FieldSize.fieldSize * FieldPosition.x + "px";
+        this.field.style.top = FieldSize.fieldSize * FieldPosition.y + "px";
         this.field.style.position = "absolute";
         this.fieldPosition = document.getElementById("drawingTable").appendChild(this.field);
 
@@ -30,36 +35,43 @@ class Field {
     }
 }
 class Table {
-
-
     constructor(yTableSize, xTableSize) {
         this.array = [];
         this.yTableSize = yTableSize;
         this.xTableSize = xTableSize;
+        
     }
+
 
     DisplayTheTable() {
         var id = 0;
+        var fieldsize=20
         for (var i = 0; i < this.yTableSize; i++) {
             this.array.push([]);
             for (var j = 0; j < this.xTableSize; j++) {
-                this.array.push(new Field(new FieldPosition(i, j, id)))
+                this.array.push(new Field(new FieldPosition(i, j, id), new FieldSize(fieldsize)))
                 id++
             }
         }
         for (var i = 0; i < this.yTableSize * this.xTableSize; i++) {
-            if (i % 2 != 0) {
-                document.getElementById(i).style.backgroundColor = "white";
-            }
-            else {
-                document.getElementById(i).style.backgroundColor = "#B6A999";
+            document.getElementById(i).style.backgroundColor = "white";
+            if (i % 2 == 0) {
+                document.getElementById(i).style.backgroundColor = "#C8E0E1";
             }
         }
+        document.getElementById("drawingTable").style.height=this.yTableSize*20+"px";
+        document.getElementById("drawingTable").style.width=this.xTableSize*20+"px";
+
     }
+
+
+
 }
 class ColorField {
     get colors() {
         return [
+            ["#FFFFFF", "#C8E0E1", "#A7BABA", "#8FA2A2", "#708181"],
+            ["#627070", "#495353", "#353D3D", "#272E2E", "#0B0D0D"],
             ["#EA2828", "#C90011", "#A80000", "#880000", "#6A0000"],
             ["#E68CE7", "#BD66BF", "#964299", "#701B74", "#4B0052"],
             ["#8C8CE7", "#6A6DC4", "#484FA2", "#233381", "#001A62"],
@@ -84,12 +96,12 @@ class ColorField {
 
         document.getElementById("colorPalette").appendChild(colorField);
         colorField.onclick = function () {
-            for (var i = 5000; i <5040; i++) {
+            for (var i = 5000; i < 5045; i++) {
                 document.getElementById(i).style.borderColor = "white";
             }
             this.style.border = "1px solid black";
-            var lofasz=this.style.backgroundColor;
-            document.getElementById("statusCanvas1").style.backgroundColor=lofasz;
+            var lofasz = this.style.backgroundColor;
+            document.getElementById("statusCanvas1").style.backgroundColor = lofasz;
         }
     }
 
@@ -98,10 +110,9 @@ class ColorField {
 class ColorTable {
     DisplayTheColorTable() {
         var id = 5000;
-        for (var i = 0; i < 8; i++) {
+        for (var i = 0; i < 9; i++) {
             for (var j = 0; j < 5; j++) {
-                var c = new ColorField(i, j, id)
-                console.log(c.colors[0].length)
+                var c = new ColorField(i, j, id);
                 id++;
             }
         }
@@ -110,7 +121,10 @@ class ColorTable {
 
 class Main {
     startTheProgram() {
-        var t = new Table(35, 35);
+        var t = new Table(27, 25);
+
+
+
         t.DisplayTheTable();
         var colorTable = new ColorTable();
         colorTable.DisplayTheColorTable();
